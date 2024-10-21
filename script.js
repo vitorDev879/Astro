d4348d1943c3f49b5db4bbdb00ddb5cb253585d4
Vitor Hugo <vferreira879@yahoo.com>
2024-10-21 14:26:17 -0300

    Importando bibliotecas

---
+++ script.js
@@ -0,0 +1,4 @@
+// Importando as bibliotecas necessarias
+const { Client, GatewayIntentBits} = require('discord.js'); // Biblioteca para conectar com o discord
+const axios = require('axios'); // Biblioteca para fazer requisicoes HTTP, acessando a API da OpenAI
+
